4973dbfc9c352e954dadc42946111b2b827d8a4c
ABBY GUO (ITA-ISDC-ISD-OOCLL/ZHA) <GUOAB@OOCL.COM>
2018-07-10 21:15:21 +0800

    [GUOAB]

--- src/main/java/com/thoughtworks/collection/Add.java
+++ src/main/java/com/thoughtworks/collection/Add.java
@@ -2,27 +2,62 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.Arrays;
+import java.util.IntSummaryStatistics;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Add {
     public int getSumOfEvens(int leftBorder, int rightBorder) {
-        throw new NotImplementedException();
+        if(leftBorder>rightBorder){
+            int temp =0;
+            temp = leftBorder;
+            leftBorder = rightBorder;
+            rightBorder = temp;
+        }
+        int sum = 0;
+        for(int i=leftBorder;i<=rightBorder;i++){
+            if(i%2==0)
+                sum +=i;
+        }
+        return sum;
     }
 
     public int getSumOfOdds(int leftBorder, int rightBorder) {
-        throw new NotImplementedException();
+        if(leftBorder>rightBorder){
+            int temp =0;
+            temp = leftBorder;
+            leftBorder = rightBorder;
+            rightBorder = temp;
+        }
+        int sum = 0;
+        for(int i=leftBorder;i<=rightBorder;i++){
+            if(i%2==1)
+                sum +=i;
+        }
+        return sum;
     }
 
     public int getSumTripleAndAddTwo(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        int sum = 0;
+        IntSummaryStatistics stats = arrayList.stream().mapToInt((x) -> x*3+2).summaryStatistics();
+        return (int)stats.getSum();
     }
 
     public List<Integer> getTripleOfOddAndAddTwo(List<Integer> arrayList) {
-        throw new NotImplementedException();
+
+        List<Integer> resultList = arrayList.stream().map( i -> {
+            if(i%2!=0) i=i*3+2;return i;
+        }).collect(Collectors.toList());
+        return  resultList;
     }
 
     public int getSumOfProcessedOdds(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        IntSummaryStatistics stats = arrayList.stream().mapToInt((x) -> {
+            if(x%2==1) x=x*3+5;
+            return x;
+        }).summaryStatistics();
+        return (int)stats.getSum();
     }
 
     public double getMedianOfEven(List<Integer> arrayList) {
