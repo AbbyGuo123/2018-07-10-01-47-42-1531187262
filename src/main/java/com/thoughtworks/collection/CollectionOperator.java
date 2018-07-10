8907660db3a9b7ea1feeb12b03e3bcd6cd570cb5
ABBY GUO (ITA-ISDC-ISD-OOCLL/ZHA) <GUOAB@OOCL.COM>
2018-07-10 23:59:29 +0800

    [GUOAB]

--- src/main/java/com/thoughtworks/collection/CollectionOperator.java
+++ src/main/java/com/thoughtworks/collection/CollectionOperator.java
@@ -2,15 +2,40 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CollectionOperator {
     public List<Integer> getListByInterval(int left, int right) {
-        throw new NotImplementedException();
+        if(left>right){
+            int temp = left;
+            left = right;
+            right = temp;
+        }
+        List<Integer> list = new ArrayList<>();
+        for(int i=right;i<=left;i--){
+            list.add(i);
+        }
+        return list;
     }
 
     public List<Integer> getEvenListByIntervals(int left, int right) {
-        throw new NotImplementedException();
+        boolean flag=false;
+        if(left>right){
+            int temp = left;
+            left = right;
+            right = temp;
+            flag=true;
+        }
+        List<Integer> list = new ArrayList<>();
+        for(int i=right;i<=left;i--){
+            if(i%2==0)
+            list.add(i);
+        }
+        if(flag)list.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
+        return list;
     }
 
     public List<Integer> popEvenElments(int[] array) {
